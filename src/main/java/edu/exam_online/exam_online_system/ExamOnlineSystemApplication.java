26c9c78f0a7b84a601c133c39647029cb6b4cd76
hoangtuyen04work <hoangtuyen04.work@gmail.com>
2025-11-08 15:13:33 +0700

    update some api

--- src/main/java/edu/exam_online/exam_online_system/ExamOnlineSystemApplication.java
+++ src/main/java/edu/exam_online/exam_online_system/ExamOnlineSystemApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableWebSecurity
 @EnableJpaAuditing
 public class ExamOnlineSystemApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(ExamOnlineSystemApplication.class, args);
 	}
 
