92f3276c1246f2ed849fc0116944325955a113bb
hoangtuyen04work <hoangtuyen04.work@gmail.com>
2026-01-16 18:16:34 +0700

    Update detail

--- src/main/java/edu/exam_online/exam_online_system/commons/constant/StudentEventEnum.java
+++ src/main/java/edu/exam_online/exam_online_system/commons/constant/StudentEventEnum.java
@@ -14,5 +14,7 @@ public enum StudentEventEnum {
     TAB_SWITCH, // Sinh viên chuyển tab
     SUBMIT, // Sinh viên nộp bài
     DISCONNECTED, // Sinh viên mất kết nối đột ngột
-    RECONNECTED // Sinh viên kết nối lại
+    RECONNECTED, // Sinh viên kết nối lại
+    FULLSCREEN_EXIT, // Sinh viên thoát chế độ toàn màn hình
+    WINDOW_RESIZE // Sinh viên thay đổi kích thước cửa sổ
 }
